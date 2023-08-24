9c7df9db640801564c80dfd8f3659af86c88ab5c
Nikita <ba999@mail.ru>
2023-08-24 20:50:55 +0300

    commit 2

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ import crud
 dg.start() # генерация базы данных
 lg.logging.info('Start') # Модуль logging («logger»), для логирования сообщений, которые  хотим видеть.
 crud.init_data_base('base_phone.csv') # Модуль работы с базой данных
-ui.ls_menu() # Модуль пользовательского интерфейса7
+ui.ls_menu() # Модуль пользовательского интерфейса
